04fb34eae2105313eb2f37684b7aefda3d5bb510
Austin  Burke <aburk3@gmail.com>
2018-11-22 02:02:48 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,7 +7,8 @@ class Song < ActiveRecord::Base
   end
 
   def drake_made_this
-    binding.pry
+    drake = Artist.find_or_create_by(name: "Drake")
+    self.artist = drake
     drake = self.artists
   end
 end
